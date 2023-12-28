c8b6babc2071a378c9c17246f8b961b4566863ed
shubhashish812 <ssing446@asu.edu>
2023-12-28 08:26:44 -0700

    Implemented PostGreSQL instead of SQLite

--- foodDelivery/settings.py
+++ foodDelivery/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'foodDelivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodOnline_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Shubhashish_8',
+        'HOST': 'localhost',
     }
 }
 
