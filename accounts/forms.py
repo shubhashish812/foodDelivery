edc94da54e8ce6613da484accbaffc91fbb9e798
shubhashish812 <ssing446@asu.edu>
2023-12-29 01:09:27 -0700

    Added data validation check for user registration and alert messages

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone_number', 'password']
+
+    def clean(self):
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("confirm_password")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match!")
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def registerUser(request):
-    return HttpResponse("User Registration Form")
+    if request.method == 'POST':
+        #print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # user = form.save(commit=False)
+            # pw = form.cleaned_data['password']
+            # user.set_password(pw)
+            # user.role = User.CUSTOMER_CHOICE
+            # user.save()
+
+            # Create user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+            user.role = User.CUSTOMER_CHOICE
+            user.save()
+            messages.success(request, 'User is Created!')
+            return redirect('registerUser')
+        else:
+            print('Invalid form')
+            #print(form.errors)
+    else:
+        form = UserForm()
+    context = {'form': form}
+    return render(request, 'accounts/registerUser.html', context)
